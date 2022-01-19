5a2befeb5b347e9fd66d71fa3ace914e6f15b623
KentaroMatsuki <95513931+kentaro425@users.noreply.github.com>
2022-01-19 02:20:28 +0000

    [Finish]app2-19

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-    resources :post_comments, only: [:create]
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
   get '/homes/about' => 'homes#about',as:"about"
